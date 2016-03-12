192e77537c8457151d185a770023e94953a176a3
Jonathan Lipman <jlipman500@gmail.com>
2016-03-11 18:30:13 -0600

    looked at differences in both files

--- findMissingFile.py
+++ findMissingFile.py
@@ -1,7 +1,10 @@
 import os
-
 for i in os.listdir('PrecipMaps'):
     file = i.split('_')[1].split('.')[0]
-
     if not os.path.exists('TempMaps/colormaxmin_' + file + '.gif'):
-       print(file)
+        print file
+
+for i in os.listdir('TempMaps'):
+    file = i.split('_')[1].split('.')[0]
+    if not os.path.exists('PrecipMaps/precip_' + file + '.gif'):
+        print file
